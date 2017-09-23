742e0bd29c4181c265e35f2c6c9268ece1f7738f
syxbyi <syxbyi97@gmail.com>
2017-09-23 20:16:29 +0800

    normalize added

--- KMeansImproved/KMeansImproved.py
+++ KMeansImproved/KMeansImproved.py
@@ -7,7 +7,8 @@ __version__ = '1.0.0'
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as sqlf
 from pyspark.ml.clustering import KMeans
-from pyspark.ml.feature import VectorAssembler
+# Import Normalizer here
+from pyspark.ml.feature import VectorAssembler, Normalizer
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -19,13 +20,23 @@ vecAssembler = VectorAssembler(inputCols = df.columns, outputCol = 'features')
 cast = [sqlf.col(x).cast('Double').alias(x) for x in vecAssembler.getInputCols()]
 df = df.select(*cast)
 #print(df.columns)
-df = vecAssembler.transform(df)
-#df.show(5)
+df = vecAssembler.transform(df).select('features')
+df.select('features').show(5, False)
 
-kmeans = KMeans(k = 2, seed = 1)
-model = kmeans.fit(df)
+# Normalizing using L1 norm
+normalizer = Normalizer(inputCol = 'features', outputCol = 'norm_features', p = 1.0)
+norm_df = normalizer.transform(df)
+# Show the features column and do not omit
+#norm_df.select('norm_features').show(5, False)
 
-wssse = model.computeCost(df)
+# Change normalizing method
+#infnorm_df = normalizer.transform(df, {normalizer.p: float('inf')})
+#infnorm_df.select('features').show(5, False)
+
+kmeans = KMeans(k = 2, seed = 1, featuresCol = 'norm_features')
+model = kmeans.fit(norm_df)
+
+wssse = model.computeCost(norm_df)
 print("Within Set Sum of Squared Errors = " + str(wssse))
 
 centers = model.clusterCenters()
